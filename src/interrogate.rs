98ce54d7fe58a89e67ee96dc929da519d9a315f5
Amr Moussa <amrmoussa100@gmail.com>
2024-04-14 11:56:12 -0400

    change interrogation functions to output to structs, and then format those structs in main. Handle threading

---
+++ src/interrogate.rs
@@ -0,0 +1,136 @@
+use nix::{
+    libc::user_regs_struct,
+    sys::{ptrace, wait},
+    unistd::Pid,
+};
+use num_traits::FromPrimitive;
+use std::collections::{HashMap, HashSet};
+use std::{thread, time::Duration};
+
+use crate::proc_fs::*;
+use crate::unistd_64::*;
+
+#[derive(Debug, Clone)]
+pub enum ProcessState {
+    Running,
+    BlockedOnRegularFileRead(RegularFileBlockInfo),
+    BlockedOnSocketRead(SocketBlockInfo),
+    BlockedOnOtherSyscall(SystemCall),
+    BlockedOnClosedSocket(u64),
+}
+
+#[derive(Debug, Clone)]
+pub struct SocketBlockInfo {
+    pub fd: u64,
+    pub remote_ip: SocketAddress,
+    pub owning_pid: Option<Pid>,
+}
+
+#[derive(Debug, Clone)]
+pub struct RegularFileBlockInfo {
+    pub fd: u64,
+    pub file_path: String,
+}
+
+pub fn execute_after_stopped(pid: Pid, mut f: impl FnMut() -> ()) {
+    match wait::waitpid(pid, None) {
+        Ok(wait::WaitStatus::Stopped(_, _)) => {
+            f();
+        }
+        _ => {
+            ptrace::detach(pid, None)
+                .unwrap_or_else(|_| panic!("Failed to detach from process {}", pid.as_raw()));
+            panic!(
+                "Process {} changed state but not to a stopped state!",
+                pid.as_raw()
+            );
+        }
+    }
+}
+
+pub fn execute_after_stopped_with_no_hang(pid: Pid, mut f: impl FnMut() -> ()) -> bool {
+    for _ in 0..4 {
+        match wait::waitpid(pid, Some(wait::WaitPidFlag::WNOHANG)) {
+            Ok(wait::WaitStatus::Stopped(_, _)) => {
+                f();
+                return true;
+            }
+            _ => {}
+        }
+        thread::sleep(Duration::from_millis(100));
+    }
+    return false;
+}
+
+pub fn analyze_syscall(
+    pid: Pid,
+    regs: user_regs_struct,
+    addr_to_socket: &HashMap<SocketAddress, Socket>,
+) -> ProcessState{
+    match FromPrimitive::from_u64(regs.orig_rax) {
+        Some(SystemCall::Read) | Some(SystemCall::Recvfrom) => {
+            // understand the file descriptor details
+            let fd = describe_fd(pid, regs.rdi).expect("Failed to get fd stats for process");
+
+            // if its a socket, handle some special logic
+            match fd.fd_type {
+                FdType::Socket => {
+                    let sockets = describe_open_sockets(pid)
+                        .expect("failed to describe sockets for the blocked process");
+                    let socket = sockets
+                        .iter()
+                        .find(|s| s.inode == fd.inode);
+                    
+                    if let Some(socket) = socket {
+                        // see if this is being served by our machine
+                        return match addr_to_socket.get(&socket.remote_ip) {
+                            Some(remote_socket) => {
+                                ProcessState::BlockedOnSocketRead(SocketBlockInfo{fd: regs.rdi, remote_ip: socket.remote_ip, owning_pid: Some(Pid::from_raw(remote_socket.owning_pid))})
+                            }
+                            _ => ProcessState::BlockedOnSocketRead(SocketBlockInfo{fd: regs.rdi, remote_ip: socket.remote_ip, owning_pid: None}),
+                        }
+                    } else {
+                        return ProcessState::BlockedOnClosedSocket(regs.rdi);
+                    }
+                }
+                FdType::Regular => {
+                    let file_path = get_path_of_regular_fd(pid, regs.rdi)
+                        .expect("Failed to get path of regular file descriptor");
+                    ProcessState::BlockedOnRegularFileRead(RegularFileBlockInfo{fd: regs.rdi, file_path})
+                }
+                _ => ProcessState::BlockedOnOtherSyscall(SystemCall::Read),
+            }
+        }
+        Some(other) => ProcessState::BlockedOnOtherSyscall(other),
+        None => ProcessState::BlockedOnOtherSyscall(SystemCall::from_u64(regs.orig_rax).expect("Could not interpret what system call was being issued")),
+    }
+}
+
+pub fn interrogate_pid_for_block(
+    pid: Pid,
+    tid: Pid,
+    addr_to_socket: &HashMap<SocketAddress, Socket>,
+    previously_interrogated_pids: &mut HashSet<i32>,
+) -> ProcessState {
+    let mut state = ProcessState::Running;
+    previously_interrogated_pids.insert(tid.as_raw());
+    ptrace::attach(tid).unwrap_or_else(|_| panic!("Could not attach to process {}", tid.as_raw()));
+    execute_after_stopped(tid, || {
+        ptrace::syscall(tid, None).unwrap_or_else(|_| {
+            panic!(
+                "Failed to wait for process {} to issue a syscall",
+                tid.as_raw()
+            )
+        });
+        execute_after_stopped_with_no_hang(tid, || match ptrace::getregs(tid) {
+            Ok(regs) => {
+                state = analyze_syscall(pid, regs, addr_to_socket)
+            }
+            Err(errno) => panic!("Get registers from process failed with code {}", errno),
+        });
+
+        ptrace::detach(tid, None)
+            .unwrap_or_else(|_| panic!("Failed to detach from process {}", tid.as_raw()));
+    });
+    state
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
 use clap::Parser;
-use nix::{
-    libc::user_regs_struct,
-    sys::{ptrace, wait},
-    unistd::Pid,
-};
-use num_traits::FromPrimitive;
-use std::collections::{HashMap, HashSet};
-use std::{thread, time::Duration};
+use nix::unistd::Pid;
+use std::collections::HashSet;
 
 mod proc_fs;
 mod unistd_64;
+mod interrogate;
 
 use proc_fs::*;
-use unistd_64::*;
+use interrogate::*;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -22,128 +17,21 @@ struct Args {
     pid: i32,
 }
 
-fn execute_after_stopped(pid: Pid, mut f: impl FnMut() -> ()) {
-    match wait::waitpid(pid, None) {
-        Ok(wait::WaitStatus::Stopped(_, _)) => {
-            f();
-        }
-        _ => {
-            ptrace::detach(pid, None)
-                .unwrap_or_else(|_| panic!("Failed to detach from process {}", pid.as_raw()));
-            panic!(
-                "Process {} changed state but not to a stopped state!",
-                pid.as_raw()
-            );
-        }
-    }
-}
-
-fn execute_after_stopped_with_no_hang(pid: Pid, mut f: impl FnMut() -> ()) -> bool {
-    for _ in 0..4 {
-        match wait::waitpid(pid, Some(wait::WaitPidFlag::WNOHANG)) {
-            Ok(wait::WaitStatus::Stopped(_, _)) => {
-                f();
-                return true;
-            }
-            _ => {}
-        }
-        thread::sleep(Duration::from_millis(100));
-    }
-    return false;
-}
-
-fn analyze_syscall(
-    pid: Pid,
-    regs: user_regs_struct,
-    addr_to_socket: &HashMap<SocketAddress, Socket>,
-    previously_interrogated_pids: &mut HashSet<i32>,
-) {
-    match FromPrimitive::from_u64(regs.orig_rax) {
-        Some(SystemCall::Read) | Some(SystemCall::Recvfrom) => {
-            // understand the file descriptor details
-            let fd = describe_fd(pid, regs.rdi).expect("Failed to get fd stats for process");
-            print!("by READ ");
-
-            // if its a socket, handle some special logic
-            match fd.fd_type {
-                FdType::Socket => {
-                    let sockets = describe_open_sockets(pid)
-                        .expect("failed to describe sockets for the blocked process");
-                    let socket = sockets
-                        .iter()
-                        .find(|s| s.inode == fd.inode)
-                        .expect("couldn't find a socket with the fd's inode");
-                    print!(
-                        "on SOCKET (file descriptor {}) which is connected to {}\n",
-                        regs.rdi, socket.remote_ip
-                    );
-
-                    // see if this is being served by our machine
-                    match addr_to_socket.get(&socket.remote_ip) {
-                        Some(remote_socket) => {
-                            // run recursively to see why our blocker is blocked
-                            print!(
-                                "\t{} is being served on this machine by process {}; ",
-                                socket.remote_ip, remote_socket.owning_pid
-                            );
-                            if !previously_interrogated_pids.contains(&remote_socket.owning_pid) {
-                                println!("Recursing...");
-                                interrogate_pid_for_block(
-                                    Pid::from_raw(remote_socket.owning_pid),
-                                    addr_to_socket,
-                                    previously_interrogated_pids,
-                                );
-                            }
-                        }
-                        _ => (),
-                    }
-                }
-                FdType::Regular => {
-                    let file_path = get_path_of_regular_fd(pid, regs.rdi)
-                        .expect("Failed to get path of regular file descriptor");
-                    print!(
-                        "on REGULAR FILE (file descriptor {}) whose path is {}\n",
-                        regs.rdi, file_path
-                    );
-                }
-                _ => (),
+fn print_process_state(process_state: &ProcessState) {
+    match process_state {
+        ProcessState::Running => print!("is Running unblocked"),
+        ProcessState::BlockedOnOtherSyscall(syscall) => print!("is blocked on system call {:?}", syscall),
+        ProcessState::BlockedOnRegularFileRead(info) => print!("is blocked reading from file {}", info.file_path),
+        ProcessState::BlockedOnSocketRead(info) => {
+            if let Some(owning_pid) = info.owning_pid {
+                print!("is blocked reading from a socket connected to {}, which is served locally by process {}", info.remote_ip, owning_pid)
+            } else {
+                print!("is blocked reading from a socket connected to {}, which is served on a remote machine", info.remote_ip)
             }
         }
-        Some(other) => println!("on {:?}", other),
-        None => println!("on SYSTEM CALL {}", regs.orig_rax),
+        ProcessState::BlockedOnClosedSocket(fd) => print!("is blocked on a socket that no longer exists (fd {})", fd)
     }
-}
-
-fn interrogate_pid_for_block(
-    pid: Pid,
-    addr_to_socket: &HashMap<SocketAddress, Socket>,
-    previously_interrogated_pids: &mut HashSet<i32>,
-) {
-    previously_interrogated_pids.insert(pid.as_raw());
-    ptrace::attach(pid).unwrap_or_else(|_| panic!("Could not attach to process {}", pid.as_raw()));
-    execute_after_stopped(pid, || {
-        ptrace::syscall(pid, None).unwrap_or_else(|_| {
-            panic!(
-                "Failed to wait for process {} to issue a syscall",
-                pid.as_raw()
-            )
-        });
-        print!("Process {} ", pid.as_raw());
-        let did_syscall = execute_after_stopped_with_no_hang(pid, || match ptrace::getregs(pid) {
-            Ok(regs) => {
-                print!("BLOCKED ");
-                analyze_syscall(pid, regs, addr_to_socket, previously_interrogated_pids)
-            }
-            Err(errno) => println!("Get registers from process failed with code {}", errno),
-        });
-
-        if !did_syscall {
-            println!("RUNNING");
-            return;
-        }
-        ptrace::detach(pid, None)
-            .unwrap_or_else(|_| panic!("Failed to detach from process {}", pid.as_raw()));
-    });
+    println!();
 }
 
 fn main() {
@@ -152,5 +40,20 @@ fn main() {
     let mut previously_interrogated_pids = HashSet::new();
     let addr_to_socket =
         describe_all_open_sockets().expect("could not read global socket information from /proc");
-    interrogate_pid_for_block(pid, &addr_to_socket, &mut previously_interrogated_pids);
+
+    let tids = get_thread_ids_for_pid(pid).unwrap_or(vec![]);
+    
+    // we want to format non-multithreaded outputs differently 
+    if tids.len() <= 1 {
+        let process_state = interrogate_pid_for_block(pid, pid, &addr_to_socket, &mut previously_interrogated_pids);
+        print!("Process {} ", pid.as_raw());
+        print_process_state(&process_state);
+    } else {
+        println!("Process {} is multithreaded.", pid.as_raw());
+        for tid in tids {
+            let process_state = interrogate_pid_for_block(pid, tid, &addr_to_socket, &mut previously_interrogated_pids);
+            print!("\tThread {} ", tid.as_raw());
+            print_process_state(&process_state);
+        }
+    }
 }
